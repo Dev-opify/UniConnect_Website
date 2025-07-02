22845a5b22bf1e0ddccd2a5b3307b518e1c3fa01
Bibhu krupa <bibhukrupa5@gmail.com>
2025-07-02 20:19:00 +0530

    Updated app.py for deplyment

--- Chatbot/app.py
+++ Chatbot/app.py
@@ -51,4 +51,5 @@ def chat():
     return jsonify({"response": bot_reply})
 
 if __name__ == "__main__":
-    app.run(debug=True)
+    port = int(os.environ.get("PORT", 5000))  # Use environment PORT or fallback to 5000
+    app.run(host="0.0.0.0",port=port, debug=True)
